9232cc339d9d13a069d683aa8eb5fcbc9a99f044
Rohit22-dev <rkflash22@gmail.com>
2023-02-09 19:42:19 +0530

    Backend setup
    Added mongo & routes, created schema and added middleware authentication using jwt

---
+++ models/Post.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const PostSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    picturePath: String,
+    location: String,
+    description: String,
+    userPicturePath: String,
+    likes: {
+      type: Map,
+      of: Boolean,
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timeStamp: true }
+);
+
+const Post = mongoose.model("Post", PostSchema);
+export default Post;
---
+++ routes/users.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  getUser,
+  getUserFriends,
+  addRemoveFriends,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+//Read
+
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+
+//Update
+
+router.patch("/:id/:friendsId", verifyToken, addRemoveFriends);
+
+export default router;
