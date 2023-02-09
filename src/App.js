4816c7559dbe9663994355c902b16a180919b1ab
Rohit22-dev <rkflash22@gmail.com>
2023-02-10 01:27:14 +0530

    UI added
    Created posts , post , profile page, friendsPage, userWidget and brandingWidget.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { themeSettings } from "./theme";
 const App = () => {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div>
@@ -19,8 +20,14 @@ const App = () => {
           <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
